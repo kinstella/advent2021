97d00a86ed76b1908cd0843633a94893217730bc
kinstelli <mark@kinstelli.com>
2021-12-01 10:35:08 -0600

    apparently incapable of commiting to a new repo

---
+++ src/advent2021/day1.clj
@@ -0,0 +1,37 @@
+(ns advent.day1
+  (:require [clojure.string :as str]))
+
+(def data (slurp "resources/day1/example.data"))
+(def theints (map #(Integer/parseInt %) (str/split data #"\n")))
+
+(defn part1 [numset]
+  (loop [remset numset
+         result 0]
+    (if (empty? remset)
+      result
+      (recur (rest remset)
+             (if
+              (and (second remset)
+                   (< (first remset) (second remset)))
+               (do
+                 (inc result))
+               result)))))
+
+(defn part2 [nums]
+  (loop [remset nums
+         inccounter 0
+         lastsum 0]
+    (if (empty? remset)
+      inccounter
+      (recur (rest remset)
+             (if
+              (and (second remset)
+                   (< lastsum (apply + (take 3 remset))))
+               (inc inccounter)
+               inccounter)
+             (apply + (take 3 remset))))))
+
+(comment
+  (part1 theints) ;;
+  (part2 theints) ;;
+  )
