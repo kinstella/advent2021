46a01ca7b9e68d444bd04ac0fd72283ecf7f332b
kinstelli <mark@kinstelli.com>
2021-12-07 00:40:53 -0600

    only params within functions

--- src/advent2021/days/day7.clj
+++ src/advent2021/days/day7.clj
@@ -6,29 +6,29 @@
 (def positions (map #(Integer/parseInt %) (str/split data #",")))
 
 ; Part 1 - constant cost
-(defn assess-fuel-cost [positions num]
+(defn assess-fuel-cost [ps num]
   (reduce + (map #(Math/abs (- num %))
-                 positions)))
+                 ps)))
 
-(defn lowest-fuel-cost-part1 []
+(defn lowest-fuel-cost-part1 [ps]
   (reduce (fn [lowest new-num]
             (min lowest new-num))
-          (for [x (range 0 (reduce max positions))]
-            (assess-fuel-cost positions x))))
+          (for [x (range 0 (reduce max ps))]
+            (assess-fuel-cost ps x))))
 
 ; Part 2 - increasing cost
-(defn assess-increasing-cost [positions num]
+(defn assess-increasing-cost [ps num]
   (reduce + (map #(let [d (Math/abs (- num %))]
-                    (apply + (range 1 (inc d)))) positions)))
+                    (apply + (range 1 (inc d)))) ps)))
 
-(defn lowest-fuel-cost-part2 []
+(defn lowest-fuel-cost-part2 [ps]
   (reduce (fn [lowest new-num]
             (min lowest new-num))
-          (for [x (range 0 (reduce max positions))]
-            (assess-increasing-cost positions x))))
+          (for [x (range 0 (reduce max ps))]
+            (assess-increasing-cost ps x))))
 
 (comment
-  (lowest-fuel-cost-part1)
-  (lowest-fuel-cost-part2)
+  (lowest-fuel-cost-part1 positions)
+  (lowest-fuel-cost-part2 positions)
 
   #_endcomment)
