98594541ce5ac809c8bc2b3f4575f6ff45e38674
mark <mark@kinstelli.com>
2022-12-10 16:35:29 -0600

    redo day6

--- src/advent2021/days/day6.clj
+++ src/advent2021/days/day6.clj
@@ -3,32 +3,7 @@
 
 (def startingfish (mapv #(Integer/parseInt %)
                         (str/split
-                         (slurp "resources/data/day6/input.data") #",")))
-
-(def babyfish (atom 0))
-
-(def fishperday (atom []))
-
-(defn dec-fish [f]
-  (let [newf (dec f)]
-    (if (< newf 0)
-      (do
-        (swap! babyfish inc)
-        6)
-      newf)))
-
-(defn fish-simulator [d startingfishv]
-  (loop [rmdays (range 0 d)
-         fishv startingfishv]
-    (reset! babyfish 0)
-    (swap! fishperday conj {(first rmdays) (count fishv)})
-    (if (empty? rmdays)
-      (println "count! " (count fishv))
-      (let [nextfish (mapv dec-fish fishv)]
-        (recur
-         (rest rmdays)
-         (reduce conj nextfish (vec (take @babyfish (repeat 8)))))))))
-
+                         (slurp "resources/data/day06/input.txt") #",")))
 
 (defn initmap [freqmap]
   (reduce (fn [newm e]
@@ -41,31 +16,27 @@
 (defn fish-count-by-num [days startingfish]
   (loop [freqmap (initmap (frequencies startingfish))
          d 0]
-    (println "day: " d " " freqmap)
     (if (>= d days)
       ;; total of fish
-      (do
-        (println "fmap: " freqmap)
-        (reduce +  (vals freqmap)))
+      (reduce + (vals freqmap))
       ;; shift map to next day
-
-      (let [newmap (reduce-kv (fn [newmap k v]
-                                (if (= k 0)
-                                  (conj newmap {6 v} {8 v})
-                                  (conj newmap {(dec k) v})))
-                              {}
-                              freqmap)]
+      (let [newmap (reduce (fn [newmap k]
+                             (assoc newmap (dec k) (+
+                                                    (get freqmap k))))
+                           {}
+                           (range 9 -1 -1))
+            newmap (assoc newmap
+                          8
+                          (get newmap -1)
+                          6 (+
+                             (get newmap 6)
+                             (get newmap -1))
+                          -1 0)]
         (recur newmap
                (inc d))))))
 
 (comment
-
-
-
-  (fish-count-by-num 18 [3 4 3 1 2])
-  (def startingmap {5 22, 1 209, 4 21, 2 29, 3 19})
-
-
-  ; part 1
-
+  
+  (fish-count-by-num 256 startingfish)
+  
   #_endcomment)
